cf118fa9bac7c82480a4253532a17d5f772dacd7
facebooker212 <facebooker3124@yahoo.com>
2022-04-20 21:46:52 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase-config';
+import './styles.css'
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [pssw, setPssw] = useState('');
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if(currentUser) {
+      setUser(currentUser);
+    }
+  });
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, pssw);
+    } catch(error){};
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, pssw);
+    } catch(error){};
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <input placeholder="Ingresa tu e-mail" onChange={(event) => {
+        setEmail(event.target.value);
+      }}></input><br/><br/>
+      <input type="password" placeholder="Ingresa una password" onChange={(event) => {
+        setPssw(event.target.value);
+      }}></input><br/><br/>
+      <button onClick={register}>Register User</button>
+      <button onClick={login}>Login User</button>
+      <button onClick={logout}>Logout User</button>
+      <span>{user?.email}</span>
     </div>
   );
 }

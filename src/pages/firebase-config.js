03b316dbb07c60d59fd11324a2ca3d60c4f8bfec
facebooker212 <facebooker3124@yahoo.com>
2022-05-03 09:04:39 -0500

    Added envinronment variables

--- src/pages/firebase-config.js
+++ src/pages/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTbSdKQ9IiPTFkDeDBYZVQkEsdXd_A0qQ",
-  authDomain: "login-sys-c993a.firebaseapp.com",
-  projectId: "login-sys-c993a",
-  storageBucket: "login-sys-c993a.appspot.com",
-  messagingSenderId: "122689581155",
-  appId: "1:122689581155:web:77fe18f12cbba7fd7dc856"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 const app = initializeApp(firebaseConfig);

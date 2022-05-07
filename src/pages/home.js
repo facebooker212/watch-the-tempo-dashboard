5b4363ab04704c80ca9c81cfb571cbcda5269670
facebooker212 <facebooker3124@yahoo.com>
2022-05-07 18:10:05 -0500

    Redesigned front-end

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,10 +12,22 @@ const Home = function() {
     window.location.href='/register'
   };
 
+  //Changes route to download when called
+  const download = async () => {
+    window.location.href='/WTT.rar'
+  };
+
+  const manual = async () => {
+    window.location.href='/user_manual.pdf'
+  };
+
   return (
       <div>
+        <h1>Watch the tempo!</h1>
         <button onClick={login}>Login User</button><br/><br/>
         <button onClick={register}>Register User</button><br/><br/>
+        <button onClick={download}>Download</button><br/><br/>
+        <button onClick={manual}>User Manual</button><br/><br/>
       </div>
     );
 };
